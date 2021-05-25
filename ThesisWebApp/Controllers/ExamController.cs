0dd92b804cf1d7f70639dc3fef72108408c81bc8
Michal Kozik <michal.kozikowski99@gmail.com>
2021-05-26 00:41:04 +0200

    Tworzenie Frontendu cz. 1

---
+++ ThesisWebApp/Controllers/ExamController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThesisWebApp.Controllers
+{
+    public class ExamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PublicExams()
+        {
+            return View();
+        }
+
+        public IActionResult DoneExams()
+        {
+            return View();
+        }
+    }
+}
