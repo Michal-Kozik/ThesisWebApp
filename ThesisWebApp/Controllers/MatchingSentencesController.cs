c1081c6b9888097e6b839056dad98595d4011bdf
Michal Kozik <michal.kozikowski99@gmail.com>
2021-08-17 21:27:35 +0200

    MatchingSentences - można wprowadzać dane i jest do nich prosta walidacja.

---
+++ ThesisWebApp/Controllers/MatchingSentencesController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThesisWebApp.Models;
+using ThesisWebApp.ViewModels;
+
+namespace ThesisWebApp.Controllers
+{
+    [Authorize(Roles = "Teacher")]
+    public class MatchingSentencesController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public MatchingSentencesController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+
+
+        private bool ValidateInputs(MatchingSentencesSettingsViewModel model)
+        {
+            for (int i = 0; i < model.NumberOfSentences; i++)
+            {
+                if (String.IsNullOrEmpty(model.SentencesFirstPart[i]) || String.IsNullOrEmpty(model.SentencesSecondPart[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Settings()
+        {
+            return View(new MatchingSentencesSettingsViewModel());
+        }
+
+        [HttpGet]
+        public IActionResult Add(int numberOfSentences, string exerciseName)
+        {
+            MatchingSentencesSettingsViewModel model = new MatchingSentencesSettingsViewModel();
+            model.NumberOfSentences = numberOfSentences;
+            model.ExerciseName = exerciseName;
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Add(MatchingSentencesSettingsViewModel model)
+        {
+            if (ValidateInputs(model))
+            {
+                // do zmiany.
+                return View(model);
+            }
+            else
+            {
+                ModelState.AddModelError("", "Wypełnij wszystkie pola!");
+                return View(model);
+            }
+        }
+    }
+}
---
+++ ThesisWebApp/ViewModels/MatchingSentencesSettingsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThesisWebApp.ViewModels
+{
+    public class MatchingSentencesSettingsViewModel
+    {
+        public MatchingSentencesSettingsViewModel()
+        {
+            SentencesFirstPart = new string[10];
+            SentencesSecondPart = new string[10];
+            UserAnswers = new string[10];
+        }
+
+        [Display(Name = "Nazwa ćwiczenia")]
+        public string ExerciseName { get; set; }
+
+        [Display(Name = "Ilość zdań")]
+        public int NumberOfSentences { get; set; }
+
+        public string[] SentencesFirstPart { get; set; }
+        public string[] SentencesSecondPart { get; set; }
+        public string[] UserAnswers { get; set; }
+    }
+}
