b677f33eebef5a9fb46c4292f7cab99704d46c9f
Michal Kozik <michal.kozikowski99@gmail.com>
2021-08-21 19:34:10 +0200

    Przy tworzeniu testów widnieje lista ćwiczeń zrobionych przez zalogowanego użytkownika. Dodatkowo można dodawać ćwiczenia do testu (użycie ciasteczek).

--- ThesisWebApp/Controllers/ExamController.cs
+++ ThesisWebApp/Controllers/ExamController.cs
@@ -1,13 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ThesisWebApp.Data;
+using ThesisWebApp.Models;
 
 namespace ThesisWebApp.Controllers
 {
+    [Authorize(Roles = "Teacher")]
     public class ExamController : Controller
     {
+        private ApplicationDbContext context = new ApplicationDbContext();
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public ExamController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+
+
+        private Task<ApplicationUser> GetCurrentUserAsync()
+        {
+            return userManager.GetUserAsync(HttpContext.User);
+        }
+
+
+
         public IActionResult Index()
         {
             return View();
@@ -28,9 +51,34 @@ namespace ThesisWebApp.Controllers
             return View();
         }
 
-        public IActionResult CreateExam()
+        [HttpGet]
+        public async Task<IActionResult> CreateExam()
         {
+            var user = await GetCurrentUserAsync();
+            ViewBag.userExercises = context.Exercises.Include(e => e.ApplicationUser).Where(e => e.ApplicationUserID == user.Id).ToList();
+            ViewBag.choosenExercises = Request.Cookies["ChoosenExercises"];
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult ShowExercise(int exerciseID)
+        {
+            var exercise = context.Exercises.Where(ex => ex.ExerciseID == exerciseID).FirstOrDefault();
+            return View(exercise);
+        }
+
+        public IActionResult AddToExam(int exerciseID)
+        {
+            if (String.IsNullOrEmpty(Request.Cookies["ChoosenExercises"]))
+            {
+                Response.Cookies.Append("ChoosenExercises", exerciseID.ToString());
+            }
+            else
+            {
+                string oldCookie = Request.Cookies["ChoosenExercises"];
+                Response.Cookies.Append("ChoosenExercises", $"{oldCookie}-{exerciseID}");
+            }
+            return RedirectToAction("CreateExam");
         }
     }
 }
